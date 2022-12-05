ae4cd1f72fe6e86091c480e8336332cebf076460
lafontaju <lafontaju@gmail.com>
2022-12-05 04:33:56 -0500

    post, put, and delete

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ const data = [
   },
 ];
 
+//middleware
+app.use(express.json());
+
 // This route gets *ALL* the users
 app.get("/api/users", (req, res) => {
   res.json(data);
@@ -34,11 +37,61 @@ app.get("/api/users/:id", (req, res) => {
   if (singleUser){
     res.json(singleUser);
   } else {
-    res.sendStatus(404).end()
+    res.status(404).end()
   }
 });
 
 // BONUS QUESTION - Add routes to implement all the CRUD operations (POST, PUT, DELETE)
+
+// POST
+app.post("/api/users/:id", (req, res) => {
+  if(req.body.id !== Number(req.params.id)){
+    console.log("hhhhaaaaa????")
+    res.status(400).end();
+  }
+
+  if(!data.find((usr) => {return usr.id === Number(req.params.id)})){
+    data.push(req.body);
+    res.end()
+  } else {
+    res.status(409).end();
+  }
+});
+
+// PUT
+app.put("/api/users/:id", (req, res) => {
+  if(req.body.id !== Number(req.params.id)){
+    res.status(400).end();
+    return;
+  }
+  
+  const requestedUser = data.find((usr) => {return usr.id === Number(req.params.id)});
+  if(requestedUser){
+    const index = data.indexOf(requestedUser);
+    if (index > -1) {
+      data.splice(index, 1);
+    }
+    data.push(req.body);
+    res.end()
+  } else {
+    data.push(req.body);
+    res.status(201).end();
+  }
+});
+
+// DELETE
+app.delete("/api/users/:id", (req, res) => {
+  const userToDelete = data.find((usr) => {return usr.id === Number(req.params.id)});
+  if(userToDelete){
+    const index = data.indexOf(userToDelete);
+    if (index > -1) {
+      data.splice(index, 1);
+    }
+    res.end()
+  } else {
+    res.status(404).end();
+  }
+});
 
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
